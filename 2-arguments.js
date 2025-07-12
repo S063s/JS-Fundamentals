2694d822b209c0a583f99b0de6866eda5d93cb27
Shaun Manda <shaunwmanda@gmail.com>
2025-07-12 16:20:35 +0300

    add const to the arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,10 @@
+const numberofarguments = process.argv.length - 2;
 if (numberofarguments ===0) {
   console.log("No argument");
 
 }if (numberofarguments ===1) {
   console.log("argument found");
-  
+
 } else {
   console.log("arguments found");
+}
