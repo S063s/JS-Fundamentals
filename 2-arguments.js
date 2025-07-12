66d56270234b34a7c7561be7d4334a97dfca0ac1
Shaun Manda <shaunwmanda@gmail.com>
2025-07-12 16:31:15 +0300

    add caps to the arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,8 +3,8 @@ if (numberofarguments.length === 0) {
   console.log("No argument");
 
 } else if (numberofarguments.length === 1) {
-  console.log("argument found");
+  console.log("Argument found");
 
 } else {
-  console.log("arguments found");
+  console.log("Arguments found");
 }
