e1c392fc9f7c5bcf17a9c1b0db018db1e0360c2a
Shaun Manda <shaunwmanda@gmail.com>
2025-07-13 13:16:40 +0300

    add one more

---
+++ 8-square.js
@@ -0,0 +1,14 @@
+const sizeArgument = process.argv[2];
+const size = parseInt(sizeArgument);
+if (isNaN(size)) {
+  console.log("Missing size");
+} else if (size <= 0) {
+    if (size > 0) {
+        for (let i =0; < size; i++) {
+            let rowString = "";
+            for (let j = 0; j < size; j++)  {
+            }
+            console.log(rowString);
+    }
+
+}
