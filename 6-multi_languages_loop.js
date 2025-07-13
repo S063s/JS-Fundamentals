5a7185d59580ccd127001db96b81a472250faac7
Shaun Manda <shaunwmanda@gmail.com>
2025-07-13 11:34:14 +0300

    remove if operator and use const message

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -3,12 +3,5 @@ const message ={
     "Python is cool",
     "JavaScript is amazing"
 };
-
-let finalOutput = "";
-if (message.length > 0) {
-    finalOutput += message[0];
-}
-for (let i = 1; i < message.length; i++){
-   finalOutput += "\n" + message[i];
-}
+const finalOutput = message.join("\n");
 console.log(finalOutput);
