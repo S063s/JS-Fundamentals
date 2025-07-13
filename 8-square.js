f779bdfdb1d35958f079eb2a3da5ddafe7305df6
Shaun Manda <shaunwmanda@gmail.com>
2025-07-13 13:22:24 +0300

    add one line of code

--- 8-square.js
+++ 8-square.js
@@ -2,11 +2,12 @@ const sizeArgument = process.argv[2];
 const size = parseInt(sizeArgument);
 if (isNaN(size)) {
   console.log("Missing size");
-} else if (size <= 0) {
+} else  {
     if (size > 0) {
         for (let i =0; < size; i++) {
             let rowString = "";
-            for (let j = 0; j < size; j++)  {
+            for (let j = 0; j < size; j++) {
+                rowString += "X";{
             }
             console.log(rowString);
     }
